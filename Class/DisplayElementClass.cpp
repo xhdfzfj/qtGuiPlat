cad574418f86f51d484d0df9e459fa5ec7a074ef
xhdfzfj <xhdwork@gmail.com>
2022-12-28 19:16:32 +0800

    同步版本

--- Class/DisplayElementClass.cpp
+++ Class/DisplayElementClass.cpp
@@ -1,6 +1,25 @@
-#include "DisplayElementClass.h"
+﻿#include "DisplayElementClass.h"
 
-DisplayElementClass::DisplayElementClass()
+/**
+ * @brief DisplayElementClass::DisplayElementClass
+ * @param pType
+ * @param X
+ * @param Y
+ * @param pQStr
+ */
+DisplayElementClass::DisplayElementClass(DisplayType_e pType, int X, int Y, QString pQStr)
+{
+    mType = pType;
+    mX = X;
+    mY = Y;
+    mDisplayStr = pQStr;
+}
+
+/**
+ * @brief DisplayElementClass::~DisplayElementClass
+ */
+DisplayElementClass::~DisplayElementClass()
 {
 
 }
+
--- Class/DisplayElementClass.h
+++ Class/DisplayElementClass.h
@@ -1,11 +1,25 @@
-#ifndef DISPLAYELEMENTCLASS_H
+﻿#ifndef DISPLAYELEMENTCLASS_H
 #define DISPLAYELEMENTCLASS_H
 
+typedef enum __DisplayType_e
+{
+    DiplayHexDataType,
+}DisplayType_e;
 
 class DisplayElementClass
 {
 public:
-    DisplayElementClass();
+    DisplayElementClass( DisplayType_e pType, int X, int Y, QString pQStr );
+    ~DisplayElementClass();
+
+private:
+    DisplayType_e mType;
+
+    int mX;
+    int mY;
+
+    QString mDisplayStr;
+
 };
 
 #endif // DISPLAYELEMENTCLASS_H
